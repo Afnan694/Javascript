00ef138701ef27ee2abf254ef33611ab9ada5d21
Afnan694 <130757873+Afnan694@users.noreply.github.com>
2024-11-30 18:55:49 +0000

    Commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,21 @@
+const name = "Afnan"
+const repoCount = 50
+
+// console.log(name+" "+repoCount);
+//New Method to Write or Concat
+
+console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+const gameName = new String('Afnan')
+// console.log(gameName[0]);
+
+console.log(gameName.__proto__); //Object banrha hai matlab Symbol  //Access karne ka Method
+
+// console.log(gameName.length);
+console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('t'));
+console.log(gameName.indexOf('n'));
+
+
+
+
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 100
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(`${balance.toString().length}`);
+// console.log(balance.toFixed(3));
+
+
+const otherNumber = 123.8966
+
+
+// console.log(otherNumber.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++Maths+++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //Minus ko plus karo
+// console.log(Math.round(4.6)); //5
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.min(4,6,5,2)); //2
+// console.log(Math.max(10,4,1,5,7)); //10
+
+
+
+const min = 10
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+ 
+
+
+
+
+
+
