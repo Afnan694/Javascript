af7a80cdcf26b8c77a22ecbdecb645fd9e9b745c
Afnan694 <130757873+Afnan694@users.noreply.github.com>
2024-11-30 15:33:46 +0000

    Commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,68 @@
+// Primitive
+    // 7 types:
+            /*
+                String,Number,Boolean,null,undefined,Symbol,BigInt
+            */
+
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId); //false
+
+
+// const bigNumber = 641846546565n
+
+
+
+
+
+
+//Reference (non primitive)
+//Array , Objects , Function
+
+//datatype is Function
+
+/*
+const heros = ["Shaktiman","naagraj","doga"];
+
+
+// CURLEY BRACES KE UNDER JO BHI hai that is called object 
+// 1ST method
+{
+    name : "Afnan"
+    age : 23
+}
+
+// 2nd Method
+
+let hero = 
+{
+    name :  "Afnan",
+    age : 55
+}
+
+console.table(hero)
+
+*/ 
+// OBJECT ENDED
+
+
+//Function started
+
+// function f1()
+// {
+
+// }
+
+const temp = null;
+console.log(typeof temp);
