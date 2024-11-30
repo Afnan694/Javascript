5a6b03426155450f44a238a000f70a83d65a6d5e
Afnan694 <130757873+Afnan694@users.noreply.github.com>
2024-11-30 14:17:20 +0000

    Convert

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,6 +28,42 @@ let score = "33"
 
 // console.log(typeof stringNumber);
 
+let value  = 3
+let negValue = -value
+
+// console.log(negValue) // 3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+  
+// let str1 = "hello"
+// let str2 = " Afnan"
+// console.log(str1+str2);
+
+
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true +); // Error
+// console.log(+""); // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 10;
+
+console.table([num1,num2,num3])
+
+
+
 
 
 
