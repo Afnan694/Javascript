68395e7a708d93be39162048b1bee51ad9e592a7
Afnan694 <130757873+Afnan694@users.noreply.github.com>
2024-11-30 13:45:11 +0000

    Conversion of Datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,30 @@
+const accId = 144;
+let accountEmail = "afnan.rashid@gmail.com"
+var accountpassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+//accId = 10; //Not Allowed Redeclaration
+
+// console.log(accId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+// accountEmail  = "Afnan@gmail.com"
+// accountpassword = "1212121"
+// accountCity = "Bengluru"
+
+// console.table([accountEmail,accountpassword,accountCity,accountState])
+
+
+
+{
+    const a1 = 10;
+}
+const a1 = 10;
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+//alert(2+3) // we ar eusing nodejs, not browser
+
+console.log(2+2);
+
+
+//number => 2 to power 53
+//bigint
+//string => '"
+//boolean => true/false
+//null => standalone value
+// undefined 
+//symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
+
+
