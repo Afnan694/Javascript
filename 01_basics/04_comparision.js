d2b85eb2379eb232314446c159a5590814cb5bbe
Afnan694 <130757873+Afnan694@users.noreply.github.com>
2024-11-30 14:30:35 +0000

    Commit

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1); // true
+// console.log( 2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+/*
+    DataType Should be Same While Converting
+*/
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+// console.log(undefined == 0);  // False
+// console.log(undefined > 0); // False
+// console.log(undefined < 0); // False
+
+
+/*
+    ===
+*/
+
+console.log("2" === 2); //false
